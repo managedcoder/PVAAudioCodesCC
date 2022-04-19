98eb7a300a9e8e85d291334a0eebe5e23c4a42db
Russ Williams <ruwill@microsoft.com>
2022-04-19 09:01:04 -0400

    Cleaned up file

--- Deployment/prepopulate_audiologos.ps1
+++ Deployment/prepopulate_audiologos.ps1
@@ -16,22 +16,6 @@ $blobContainerName = "audiologos"
 $audiologoBlobName = "audiologo.wav"
 $tadaBlobName = "tada.wav"
 
-# ---------------------------
-# Create Storage account and explicitly set it to allow public access to the blobs it will contain (access level is only defined at the container level, not blob level)
-# Storage names must be between 3 and 24 characters long and use numbers and lower-case letters only
-#$storageAccountName = $name.ToLower()
-#Write-Host "Creating the Storage Account for customer service Function App and audio logos" -NoNewline -ForegroundColor Green
-#if ($showCommands.ToLower() -eq "true") { Write-Host ""; Write-Host "az storage account create -n ""$storageAccountName"" -l ""$location"" -g ""$resourceGroup"" --sku ""Standard_LRS"" " -NoNewline }
-#az storage account create `
-#  -n "$storageAccountName" `
-#  -l "$location" `
-#  -g "$resourceGroup" `
-#  --allow-blob-public-access true `
-#  --sku "Standard_LRS" `
-#  2>> "$logFile" | Out-Null
-#Write-Host " - Done." -ForegroundColor Green
-# ---------------------------
-
 # Get the az login user name
 $accInfo = az account show | ConvertFrom-Json
 
@@ -56,14 +40,6 @@ az storage container create `
     --auth-mode login `
     3>&1 2>&1 >> $logFile | Out-Null
 Write-Host " - Done." -ForegroundColor Green
-
-    #az storage container create \
-    #--account-name <account-name> \
-    #--name <container-name> \
-    #--resource-group <resource-group>
-    #--public-access blob \
-    #--account-key <account-key> \
-    #--auth-mode key
 
 # Upload audiologo.wav to storage container
 Write-Host "Upload audiologo.wav to storage container" -NoNewline -ForegroundColor Green
